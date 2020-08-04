0eb41255f9ab977af875222f814fe488f6ebe6cf
Mithilesh Kumar Thakur <mithilesh.hisp@gmail.com>
2020-08-04 14:36:03 +0530

    add develop branch

--- src/main/java/org/beehyv/dhis2openimis/adapter/Tester.java
+++ src/main/java/org/beehyv/dhis2openimis/adapter/Tester.java
@@ -1,7 +1,5 @@
 package org.beehyv.dhis2openimis.adapter;
 
-import java.time.LocalDate;
-import java.util.Arrays;
 import org.beehyv.dhis2openimis.adapter.dhis.fetch.*;
 import org.beehyv.dhis2openimis.adapter.openimis.APICaller;
 
@@ -10,6 +8,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -74,28 +74,7 @@ public class Tester{
 		
 		apiCaller.getClaimAndClaimResponseBundleAndPostToDhis(imisClaimUrl + "?" + ParamsUtil.REF_DATE_PARAM + "&" + ParamsUtil.CLAIM_PAGE_SIZE, 
 				imisClaimResponseUrl + "?" + ParamsUtil.REF_DATE_PARAM+ "&" + ParamsUtil.CLAIM_PAGE_SIZE, 105);
-                
-                
-                // orgUnit sync code
-                /*
-                logger.info("Application started with command-line arguments: {}", Arrays.toString(args.getSourceArgs()));
-                logger.info("NonOptionArgs: {}", args.getNonOptionArgs());
-                
-                
-                boolean orgUnitSync = args.containsOption("orgUnitSync");
-                logger.info("Contains orgUnitSync : " + orgUnitSync );
-                
-                if( orgUnitSync ){
-                    String openIMISLegacyDemoURL = "http://legacy.demo.openimis.org/rest/api/master";
-                    System.out.println( "imisLocationUrl -- orgUnit sync " + openIMISLegacyDemoURL + " date - " + LocalDate.of(1900, 1, 1).toString() );
-                    apiCaller.getLegacyDdemoOpenIMISOrgAndPostToDhis2(openIMISLegacyDemoURL);
-                }
-                else{
-                    System.out.println( "imisLocationUrl -- data sync " + imisLocationUrl);
-                    System.out.println( "imisClaimResponseUrl -- data sync " + imisClaimResponseUrl);
-                }                
-		*/
-                
+		
 	}
 
 }
